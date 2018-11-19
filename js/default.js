dc49192984a78e319ef4592331ae5c7337b9aa78
finoa0317 <38421089+finoa0317@users.noreply.github.com>
2018-11-19 16:50:33 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,1 +1,19 @@
-var separate_time = 
+var separate_time = funcition(time){
+  var sec   = time.getSeconds();
+  var min   = time.getMinutes();
+  var hours = time.getHours();
+  var days  = time.getDate();
+  var month = time.getMonth();
+  var year  = time.getFullYear();
+  return [sec, min, hours, days, month, year];
+}
+
+var now = new Date();
+varcounter = separate_time(now);
+document.getElementById('countdown').textCountent =
+  counter[5] + '年' +
+  counter[4] + '月' +
+  counter[3] + '日' +
+  counter[2] + '時' +
+  counter[1] + '分' +
+  counter[0] + '秒' ;
